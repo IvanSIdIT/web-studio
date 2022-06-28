10f73c724ee51467cec1f565cd68f20fba118e3a
Ivan <sidelnicov2006@gmail.com>
2022-06-28 14:54:20 +0300

    upd

--- js/srcipts.js
+++ js/srcipts.js
@@ -1,6 +1,6 @@
 //Начало великого пути
 
-const name = 'Ivan';
-let age = 16;
+// const name = 'Ivan';
+// let age = 16;
 
-console.log('Возраст пользователя: ' + age + ' Имя пользователя: ' + name);
+// console.log('Возраст пользователя: ' + age + ' Имя пользователя: ' + name);
